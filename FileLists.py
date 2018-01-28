4ab99b7299c32329ab75eb7351559f3955be78d8
Ahmad A <bbpgrs@gmail.com>
2018-01-27 23:20:25 -0500

    Improve code formatting

--- FileLists.py
+++ FileLists.py
@@ -33,26 +33,25 @@ def genFileList(reqType, fromParam=0):
     :param fromParam: Number start index for requested files
     :return:
     """
-    if reqType != "RNA" and reqType != "miRNA":
-        print("genFileList: reqType has to be 'RNA' or 'miRNA', terminating...")
-        return
 
     params = json.load(open("json/file-list_req/%s-seq.json" % reqType, 'rb'))
     params["size"] = str(FILES_PER_LIST)
     params["from"] = str(fromParam)
 
     print("Requesting information for files %(start)s to %(end)s (index+1) ..." % {
-        "start": int(fromParam)+1,
-        "end": int(fromParam)+FILES_PER_LIST
+        "start": fromParam+1,
+        "end": fromParam+FILES_PER_LIST
     })
+
     r = requests.post(URL, data=json.dumps(params), headers=HEADERS)
     print(r)
 
     fpath = "%(dir)s%(req)s-seq_%(num)s.json" % {
         "dir": FILE_LIST_DIR,
         "req": reqType,
-        "num": int(fromParam)//FILES_PER_LIST
+        "num": fromParam//FILES_PER_LIST
     }
+
     print("Writing list to %s ..." % fpath)
     f = open(fpath, 'w')
     f.write(r.text)
@@ -60,25 +59,40 @@ def genFileList(reqType, fromParam=0):
 
 
 # generate RNA file lists
+
 n = TOTAL_RNA // FILES_PER_LIST
 print("\nGenerating RNA files lists to '%s' ... " % FILE_LIST_DIR)
+
 for i in range(n):
     print("\nGenerating file list %(i)s of %(n)s ..." % {
         "i": i+1,
         "n": n
     })
-    genFileList("RNA", i*FILES_PER_LIST)
+
+    try:
+        genFileList("RNA", i*FILES_PER_LIST)
+    except TypeError:
+        print("ERROR: genFileList - invalid parameter types")
+
 print("\nRNA file lists generated")
 
+
 # generate miRNA file lists
+
 m = TOTAL_MIRNA // FILES_PER_LIST
 print("\nGenerating miRNA files lists to '%s' ... " % FILE_LIST_DIR)
+
 for i in range(m):
     print("\nGenerating file list %(i)s of %(n)s ..." % {
         "i": i+1,
         "n": m
     })
-    genFileList("miRNA", i * FILES_PER_LIST)
+
+    try:
+        genFileList("miRNA", i * FILES_PER_LIST)
+    except TypeError:
+        print("ERROR: genFileList - invalid parameter types")
+
 print("\nmiRNA file lists generated")
 
 print()
