bd68c7eadaea38c15b64ee23ed3a0fdcb1b81d92
Ahmad A <bbpgrs@gmail.com>
2018-01-27 23:05:17 -0500

    Improve print statement formatting in FileLists.py, and add Files.py

--- FileLists.py
+++ FileLists.py
@@ -1,6 +1,11 @@
 import requests
 import json
 
+"""
+A script to generate a list of GDC RNA-seq or miRNA-seq files to be downloaded 
+later using the accompanying Files.py
+"""
+
 # GDC API files endpoint URL
 URL = 'https://api.gdc.cancer.gov/files'
 
@@ -14,6 +19,9 @@ TOTAL_RNA, TOTAL_MIRNA = 35000, 25000
 
 # max files per json list
 FILES_PER_LIST = 500
+
+# file list directory
+FILE_LIST_DIR = 'json/file-list/'
 
 
 def genFileList(reqType, fromParam=0):
@@ -29,16 +37,23 @@ def genFileList(reqType, fromParam=0):
         print("genFileList: reqType has to be 'RNA' or 'miRNA', terminating...")
         return
 
-    params = json.load(open("json/file-list_req/"+reqType+"-seq.json", 'rb'))
+    params = json.load(open("json/file-list_req/%s-seq.json" % reqType, 'rb'))
     params["size"] = str(FILES_PER_LIST)
     params["from"] = str(fromParam)
 
-    print("Requesting information for files", int(fromParam)+1, "to", int(fromParam)+FILES_PER_LIST, "(index+1) ...")
+    print("Requesting information for files %(start)s to %(end)s (index+1) ..." % {
+        "start": int(fromParam)+1,
+        "end": int(fromParam)+FILES_PER_LIST
+    })
     r = requests.post(URL, data=json.dumps(params), headers=HEADERS)
     print(r)
 
-    fpath = "json/file-list/"+reqType+"-seq_"+str(int(fromParam)//FILES_PER_LIST)+".json"
-    print("Writing list to", fpath, "...")
+    fpath = "%(dir)s%(req)s-seq_%(num)s.json" % {
+        "dir": FILE_LIST_DIR,
+        "req": reqType,
+        "num": int(fromParam)//FILES_PER_LIST
+    }
+    print("Writing list to %s ..." % fpath)
     f = open(fpath, 'w')
     f.write(r.text)
     print("List written to file.")
@@ -46,17 +61,23 @@ def genFileList(reqType, fromParam=0):
 
 # generate RNA file lists
 n = TOTAL_RNA // FILES_PER_LIST
-print("\nGenerating RNA files lists to 'json/file-list/' ... ")
+print("\nGenerating RNA files lists to '%s' ... " % FILE_LIST_DIR)
 for i in range(n):
-    print("\nGenerating file list", i+1, "of", n, "...")
+    print("\nGenerating file list %(i)s of %(n)s ..." % {
+        "i": i+1,
+        "n": n
+    })
     genFileList("RNA", i*FILES_PER_LIST)
 print("\nRNA file lists generated")
 
 # generate miRNA file lists
 m = TOTAL_MIRNA // FILES_PER_LIST
-print("\nGenerating miRNA files lists to 'json/file-list/' ... ")
+print("\nGenerating miRNA files lists to '%s' ... " % FILE_LIST_DIR)
 for i in range(m):
-    print("\nGenerating file list", i+1, "of", m, "...")
+    print("\nGenerating file list %(i)s of %(n)s ..." % {
+        "i": i+1,
+        "n": m
+    })
     genFileList("miRNA", i * FILES_PER_LIST)
 print("\nmiRNA file lists generated")
 
